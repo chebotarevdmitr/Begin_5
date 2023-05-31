8f663a25319c34299abab8e9b34f1d8d48bdebda
DIM <4ebotarov_dim@inbox.ru>
2023-05-31 21:59:21 +0300

    nested if/else

--- Begin_5..cpp
+++ Begin_5..cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main() {
 	printf("Hello Nested if/else\n");
+  /*
+    User enters side lengths of a traingle (A,B,C)
+    пользователь вводит длинну сторон треугольника (А,В,С)
+    Program should write out whether that traingle is eqvilateral,isosceles,or scalene
+    Программа должна записать является этот треугольник равносторонним,равнобедренным или скалярным
+  */
+  float a,b,c;
+  cout<<"Enter A,B,C ";
+  cin>>a>>b>>c;
+  if(a==b&&b==c) 
+  	cout<<" eqvilateral traingle ";//равносторонний треугольник
+   	
+	else
+	{
+		if(a!=b&&b!=c&&c!=a)
+		cout<<" scalene traingle ";//скалярный  треугольник	
+		else
+		cout<<"isosceles  traingle ";//равнобедренный  треугольник	
+		
+	}
+	
 	
 	
 	system("pause>0");
